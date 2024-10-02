980e1dbc9ff092da7ff04ae4869e1687f5d029bd
Radek Morong <radekmorong@email.cz>
2024-10-02 11:37:08 +0200

    2.10.2024 backup

--- app/not-found.js
+++ app/not-found.js
@@ -14,7 +14,7 @@ export default function NotFound() {
   return (
     <div className="max-w-screen-xl mb-10 items-center flex flex-col flex-wrap my-4 px-4 sm:px-6 md:px-8 lg:px-0 h-screen  w-full dark:bg-[#161616] ">
       <div className="mt-10 text-2xl text-orange-600 ">
-        STRÁNKA s názvem:  <span className='underline'>'{pathName}'</span>   NENALEZENA
+        STRÁNKA s názvem:  <span className='underline'>&apos;{pathName}&apos;</span>   NENALEZENA
       </div>
       <div className="mt-10 text-2xl text-gray-800 dark:text-gray-200  ">
         <Link className='border-b-[1px] border-gray-300 dark:border-gray-600' href={'/'}>tímto odkazem se vrátíte zpět na hlavní stránku</Link>
